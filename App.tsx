47ec0a8655ceb6ff351d1282c17278f1c51bf472
Developer <dev@reaction.local>
2025-11-26 15:07:07 +0500

    fx

--- App.tsx
+++ App.tsx
@@ -1,29 +1,28 @@
-
-import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './shared/context/ThemeContext';
-import { CartProvider } from './shared/context/CartContext';
-import { AppLayout } from './components/Layout/AppLayout';
-import { Home } from './pages/Home';
-import { ProductDetails } from './pages/ProductDetails';
-import { Admin } from './pages/Admin';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./shared/context/ThemeContext";
+import { CartProvider } from "./shared/context/CartContext";
+import { Home } from "./pages/Home";
+import { ProductDetails } from "./pages/ProductDetails";
+import { Admin } from "./pages/Admin";
+import { AppLayout } from "./server/components/Layout/AppLayout";
 
 const App: React.FC = () => {
-  return (
-    <ThemeProvider>
-      <CartProvider>
-        <HashRouter>
-          <AppLayout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/product/:id" element={<ProductDetails />} />
-              <Route path="/admin" element={<Admin />} />
-            </Routes>
-          </AppLayout>
-        </HashRouter>
-      </CartProvider>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+			<CartProvider>
+				<HashRouter>
+					<AppLayout>
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/product/:id" element={<ProductDetails />} />
+							<Route path="/admin" element={<Admin />} />
+						</Routes>
+					</AppLayout>
+				</HashRouter>
+			</CartProvider>
+		</ThemeProvider>
+	);
 };
 
 export default App;
